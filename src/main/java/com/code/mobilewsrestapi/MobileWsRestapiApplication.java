2a82ccc2c593dba3dab2cb96a694af1418c3626c
Amit Kumar Singh <singhamit3472@gmail.com>
2020-06-01 13:40:12 +0530

    First commit

---
+++ src/main/java/com/code/mobilewsrestapi/MobileWsRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.code.mobilewsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileWsRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileWsRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/mobilewsrestapi/MobileWsRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.mobilewsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileWsRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
